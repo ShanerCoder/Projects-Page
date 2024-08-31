1a9dc730e9ad0e6680ce8b86a3cf0d8439d5696e
Shane Rafferty <shaner9@hotmail.co.uk>
2024-08-31 20:38:48 +0100

    Undoing PC changes and font size update

--- components/For Fun Designs/Project Clicker/PC.js
+++ components/For Fun Designs/Project Clicker/PC.js
@@ -1,46 +1,13 @@
-import { Col, Row } from "react-bootstrap";
-import { useClickerContext } from "../../../Context/ClickerProvider";
 import classes from "./PC.module.css";
 import { useState } from "react";
 
 function ProjectClicker() {
-  const { clicks, setClicks } = useClickerContext();
-  const [costOfUpgrade, setCostOfUpgrade] = useState(10);
-  const [upgradeClicks, setUpgradeClicks] = useState(1);
-
-  function upgradeClicksHandler() {
-    if (clicks >= costOfUpgrade) {
-      setClicks(clicks - costOfUpgrade);
-      setUpgradeClicks(upgradeClicks + 1);
-      setCostOfUpgrade(costOfUpgrade * 2);
-    }
-  }
+  const { clicks, setClicks } = useState(0);
 
   return (
     <>
       <div style={{ textAlign: "center" }}>
         <h1 style={{ textAlign: "center" }}>Clicks: {clicks}</h1>
-        <div>
-          <button
-            className={classes.clickerButton}
-            onClick={() => setClicks(clicks + upgradeClicks)}
-          >
-            Click Me
-          </button>
-        </div>
-
-        {clicks >= 10 && (
-          <Row>
-            <Col xs={6} md={4}>
-              <button
-                className={classes.upgradeButton}
-                onClick={upgradeClicksHandler}
-              >
-                Upgrade Clicks - Cost: {costOfUpgrade}
-              </button>
-            </Col>
-          </Row>
-        )}
       </div>
     </>
   );
